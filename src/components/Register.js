96dc4ba2c167fc20467249d3914b57af0ce02ad5
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-05-12 11:48:50 +0100

    testing out forms

---
+++ src/components/Register.js
@@ -0,0 +1,34 @@
+import { useFormik } from 'formik'
+import React from 'react'
+import styles from './styles/Register.js'
+
+
+const Register = () => {
+    const 
+    const date = new Date()
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            dateOfBirth: date.getDay(),
+            gender: '',
+            username: '',
+            password: '',
+            confirmPassword: ''
+        }, 
+        onSubmit: () => {
+
+        }
+    })
+  return (
+    <div>
+        <form onSubmit={formik.handleSubmit}>
+            <div className=''>
+
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Register
---
+++ src/useFetch.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState("");
+  const [pending, serPending] = useState(false);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    serPending(true);
+    axios
+      .get(url)
+      .then((response) => setData(response.data))
+      .then(serPending(false))
+      .catch((err) => setError(err.message));
+  });
+
+  return {data, pending, error}
+};
+
+export default useFetch;
