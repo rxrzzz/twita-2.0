8b57bb93d36820414aba2db7b6c0bbef3f2a1354
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-05-15 09:56:12 +0100

    creating comments

--- src/components/home/Comments.js
+++ src/components/home/Comments.js
@@ -1,9 +1,28 @@
+import { useFormik } from 'formik'
 import React from 'react'
 
 const Comments = ({post}) => {
-    
+  const date = new Date()
+    const formik = useFormik({
+        initialValues: {
+            commentCreator: '',
+            dateCommented: date.toLocaleString(),
+            commentContent: '',
+                                    
+        },
+        onSubmit: () => {
+          void 0
+        }
+    })
+
   return (
-    <div>Comments</div>
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        {post && <h1>Replying to {post.creator}</h1>}
+          <textarea name="commentCreator" value={formik.values.commentCreator} cols="4" rows="20" onChange={formik.handleChange} onBlur={formik.handleBlur}></textarea>
+          <button type='submit'>Comment</button>
+      </form>
+    </div>
   )
 }
 
