927195b14fa67d1d18d9923455a1b27efe902764
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-05-15 19:25:42 +0100

    styling forms

--- src/components/Register.js
+++ src/components/Register.js
@@ -44,60 +44,60 @@ const Register = () => {
         .post("http://localhost:7000/people", {
           ...formik.values,
           dpUrl: dpUrl,
-        }).then(history('/login'))
+        })
+        .then(history("/login"));
     },
   });
 
-  return (
-
-    /*Read up on Formik from their website:  https://formik.org ,
+  /*Read up on Formik from their website:  https://formik.org ,
     or watch this Youtube tutorial: https://www.youtube.com/watch?v=bMSHmf_ckM8 */
 
-    <div className={styles.register}>
-      <div className={styles.image}>
-        <img src={image} alt="#" />
-      </div>
-      <div className={styles.form_section}>
-        <div className={styles.dp_section}>
-          <div className={styles.dp}>
-            <img src={dpUrl} alt="" width="150px" height="150px" />
+  //   <div className={styles.dp_form}>
+
+  // </div>
+
+  return (
+    <div className={styles.registration_form}>
+      <div className={styles.dp_form}>
+        <div className={styles.dp}>
+          <img src={dpUrl} alt="" width="150px" height="150px" />
+        </div>
+        <div className={styles.dp_content}>
+          <div>
+            <label htmlFor="profilePicStyle">Picture Type</label>
+            <select
+              name="profilePicStyle"
+              onChange={(e) => setDpStyle(e.target.value)}
+            >
+              <option value="adventurer">Adventurer</option>
+              <option value="adventurer-neutral">Adventurer Neutral</option>
+              <option value="avataaars">Avatars</option>
+              <option value="big-ears">Big Ears</option>
+              <option value="big-ears-neutral">Big Ears Neutral</option>
+              <option value="big-smile">Big Smile</option>
+              <option value="bottts">Bottts</option>
+              <option value="croodles">Croodles</option>
+              <option value="croodles-neutral">Croodles Neutral</option>
+              <option value="identicon">Identicon</option>
+              <option value="micah">Micah</option>
+              <option value="miniavs">Miniavs</option>
+              <option value="open-peeps">Open Peeps</option>
+              <option value="personas">Personas</option>
+              <option value="pixel-art">Pixel Art</option>
+              <option value="pixel-art-neutral">Pixel Art Neutral</option>
+            </select>
           </div>
-          <div className={styles.dp_form}>
-            <div>
-              <label htmlFor="profilePicStyle">Picture Type</label>
-              <select
-                name="profilePicStyle"
-                onChange={(e) => setDpStyle(e.target.value)}
-              >
-                <option value="adventurer">Adventurer</option>
-                <option value="adventurer-neutral">Adventurer Neutral</option>
-                <option value="avataaars">Avatars</option>
-                <option value="big-ears">Big Ears</option>
-                <option value="big-ears-neutral">Big Ears Neutral</option>
-                <option value="big-smile">Big Smile</option>
-                <option value="bottts">Bottts</option>
-                <option value="croodles">Croodles</option>
-                <option value="croodles-neutral">Croodles Neutral</option>
-                <option value="identicon">Identicon</option>
-                <option value="micah">Micah</option>
-                <option value="miniavs">Miniavs</option>
-                <option value="open-peeps">Open Peeps</option>
-                <option value="personas">Personas</option>
-                <option value="pixel-art">Pixel Art</option>
-                <option value="pixel-art-neutral">Pixel Art Neutral</option>
-              </select>
-            </div>
-            <div>
-              <label htmlFor="color">Color</label>
-              <input
-                type="color"
-                name="color"
-                onChange={(e) => setDpColor(e.target.value)}
-              />
-            </div>
+          <div>
+            <label htmlFor="color">Color</label>
+            <input
+              type="color"
+              name="color"
+              onChange={(e) => setDpColor(e.target.value)}
+            />
           </div>
         </div>
-
+      </div>
+      <main className={styles.details_form}>
         <form onSubmit={formik.handleSubmit} className={styles.form}>
           <div>
             <label htmlFor="firstName">First Name</label>
@@ -168,10 +168,14 @@ const Register = () => {
           <button className={styles.button} type="submit">
             Submit
           </button>
-          <p>Already have an account? <Link to='/login'>Login.</Link></p>
+          <p>
+            Already have an account? <Link to="/login">Login.</Link>
+          </p>
         </form>
-
-      </div>
+        <div className={styles.decorative_image}>
+          <img src="" alt="" />
+        </div>
+      </main>
     </div>
   );
 };
