cec4ec41d21460c3b5bbdaf0a24133cdb6683499
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-05-16 10:50:29 +0100

    styling header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../styles/Header.module.css";
 
-const Header = ({person}) => {
+const Header = ({ person }) => {
   return (
     <div>
+      <div className={styles.person_details}>
         {person.firstName}
         {person.lastName}
-        <img src={person.dpUrl} alt="Profile Picture" width='150px'/>
-        <Link to='/create_post'>Create Post</Link>
+      </div>
+      <div className={styles.create_post}>
+        
+      </div>
+      <img src={person.dpUrl} alt="Profile Picture" width="150px" />
+      <Link to="/create_post">Create Post</Link>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,8 @@ import useFetch from "../useFetch";
 import Header from "./Header";
 import PostList from "./PostList";
 import styles from "../styles/Home.module.css";
+import image from "../images/logo.png"
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const person = JSON.parse(localStorage.getItem("personInStorage"));
@@ -12,12 +14,15 @@ const Home = () => {
 
   return (
     <div>
+      <div className={styles.app_header}>
+          <Link to='/home'><img src={image} alt="Home"/></Link>
+      </div>
       <div className={styles.header}>
         <Header person={person} />
       </div>
-      <div className={styles.postlist}>
+      <main className={styles.postlist}>
         <PostList posts={posts} error={postsError} />
-      </div>
+      </main>
     </div>
   );
 };
