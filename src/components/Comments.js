2d3704bdf8bd8e297f0294ed1a81c33a226d4e2a
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-05-16 21:26:58 +0100

    comments styling

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,45 +1,52 @@
-import axios from 'axios'
-import { useFormik } from 'formik'
-import React from 'react'
-import { useParams } from 'react-router'
+import axios from "axios";
+import { useFormik } from "formik";
+import React from "react";
+import { useParams } from "react-router";
+import styles from "../styles/Comment.module.css";
 
-const Comments = ({post}) => {
-  const {id} = useParams()
-  const person = JSON.parse(localStorage.getItem("personInStorage"))
-  const date = new Date()
+const Comments = ({ post }) => {
+  const { id } = useParams();
+  const person = JSON.parse(localStorage.getItem("personInStorage"));
+  const date = new Date();
 
   const validate = (values) => {
-    const errors = {}
-    if (!values.commentContent){
-      errors.commentContent = 'Enter a comment'
+    const errors = {};
+    if (!values.commentContent) {
+      errors.commentContent = "Enter a comment";
     }
-    return errors
-  }
-    const formik = useFormik({
-        initialValues: {
-            commentCreator: person.username,
-            dateCommented: date.toLocaleString(),
-            commentContent: '',
-            postCommentedOn: id
-                                    
-        },
-        onSubmit: () => {
-          alert(JSON.stringify(formik.values))
-          axios.post("http://localhost:3020/comments", formik.values)
-          console.log('Done')
-        }
-    })
+    return errors;
+  };
+  const formik = useFormik({
+    initialValues: {
+      commentCreator: person.username,
+      dateCommented: date.toLocaleString(),
+      commentContent: "",
+      postCommentedOn: id,
+    },
+    onSubmit: () => {
+      alert(JSON.stringify(formik.values));
+      axios.post("http://localhost:3020/comments", formik.values);
+      console.log("Done");
+    },
+  });
 
   return (
-    <div>
-      <form onSubmit={formik.handleSubmit}>
-        {post && <h1>Replying to {post.creator}</h1>}
-          <textarea name="commentContent" value={formik.values.commentContent} cols="4" rows="20" onChange={formik.handleChange} onBlur={formik.handleBlur}></textarea>
-          {formik.errors.commentContent && formik.touched.commentContent && <p>{formik.errors.commentContent}</p>}
-          <button type='submit'>Comment</button>
-      </form>
-    </div>
-  )
-}
+    <form onSubmit={formik.handleSubmit} className={styles.form}>
+      {post && <h2>Replying to {post.creator}</h2>}
+      <input
+      className={styles.comment_content}
+        name="commentContent"
+        placeholder="write a comment"
+        value={formik.values.commentContent}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      ></input>
+      {formik.errors.commentContent && formik.touched.commentContent && (
+        <p>{formik.errors.commentContent}</p>
+      )}
+      <button type="submit">Comment</button>
+    </form>
+  );
+};
 
-export default Comments
+export default Comments;
--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -2,25 +2,47 @@ import React from "react";
 import { useParams } from "react-router";
 import useFetch from "../useFetch";
 import CommentList from "./CommentList";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/PostDetails.module.css";
 import Comments from "./Comments";
 
 const PostDetails = () => {
   const { id } = useParams();
   const personInStorage = JSON.parse(localStorage.getItem("personInStorage"));
   const { data: post, error } = useFetch("http://localhost:3010/posts/" + id);
-  const {data: comments, postsError} = useFetch('http://localhost:3020/comments')
+  const { data: comments, postsError } = useFetch(
+    "http://localhost:3020/comments"
+  );
   return (
     <div>
       {error && <p>{error}</p>}
       {post && (
-        <div >
-          {post.postContent} {post.creatorDp}
+        <div className={styles.post}>
+          <div className={styles.post_header}>
+            <img src={post.creatorDp} alt={post.creator} />
+            <div>
+              <h2>
+                {post.creatorFirstName} {post.creatorLastName}
+              </h2>
+              <p>@{post.creator}</p>
+            </div>
+          </div>
+          <div className={styles.post_content}>
+            <h2>{post.postContent}</h2>
+            <div>
+              <p>{post.dateCreated}</p>
+              {personInStorage.username == post.creator && <button>Delete</button>}
+            </div>
+          </div>
+          <div className={styles.post_footer}>
+            <p>Like</p>
+            <p>Comment</p>
+            <p>Share</p>
+          </div>
         </div>
       )}
-      {personInStorage.username == post.creator && <button>Delete</button>}
-      <Comments post={post}/>
-      <CommentList comments={comments} post={post} error={postsError}/>
+
+      <Comments post={post} />
+      <CommentList comments={comments} post={post} error={postsError} />
     </div>
   );
 };
